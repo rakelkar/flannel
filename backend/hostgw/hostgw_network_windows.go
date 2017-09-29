3629bf284a6c1a808f02be8ed2a9e7f76dab9321
Rakesh Kelkar <rakelkar@gmail.com>
2017-09-28 21:58:20 -0700

    handle reinstating lost routes and updating the gateway on existing routes. also format the file

--- backend/hostgw/hostgw_network_windows.go
+++ backend/hostgw/hostgw_network_windows.go
@@ -31,6 +31,11 @@ import (
 	psbe "github.com/gorillalabs/go-powershell/backend"
 
 	"fmt"
+	"regexp"
+	"strings"
+	"bytes"
+	"bufio"
+	"strconv"
 )
 
 type network struct {
@@ -43,11 +48,11 @@ type network struct {
 }
 
 type route struct {
-	linkIndex int
+	linkIndex         int
 	destinationSubnet *net.IPNet
-	gatewayAddress net.IP
-	routeMetric int
-	ifMetric int
+	gatewayAddress    net.IP
+	routeMetric       int
+	ifMetric          int
 }
 
 func (n *network) Lease() *subnet.Lease {
@@ -88,28 +93,6 @@ func (n *network) Run(ctx context.Context) {
 		}
 	}
 }
-func runScript(cmdLine string) string {
-	// choose a PS backend
-	back := &psbe.Local{}
-
-	// start a local powershell process
-	shell, err := ps.New(back)
-	if err != nil {
-		panic(err)
-	}
-	defer shell.Exit()
-
-	log.Infof("Executing: %v", cmdLine)
-	stdout, stderr, err := shell.Execute(cmdLine)
-	if err != nil {
-		panic(err)
-	}
-
-	log.Infof("stdout [%v], stderr [%v]", stdout, stderr)
-
-	return stdout
-}
-
 func (n *network) handleSubnetEvents(batch []subnet.Event) {
 	for _, evt := range batch {
 		switch evt.Type {
@@ -121,19 +104,34 @@ func (n *network) handleSubnetEvents(batch []subnet.Event) {
 				continue
 			}
 
-			// TODO: rakesh: make this more efficient and handle cases where the route already exists for a different gw
 			route := route{
-				destinationSubnet:       evt.Lease.Subnet.ToIPNet(),
-				gatewayAddress:        evt.Lease.Attrs.PublicIP.ToIP(),
-				linkIndex: n.linkIndex,
+				destinationSubnet: evt.Lease.Subnet.ToIPNet(),
+				gatewayAddress:    evt.Lease.Attrs.PublicIP.ToIP(),
+				linkIndex:         n.linkIndex,
 			}
 
 			getRouteCmdLine := fmt.Sprintf("get-netroute -InterfaceIndex %v -DestinationPrefix %v -erroraction Ignore", route.linkIndex, route.destinationSubnet.String())
-			stdout := runScript(getRouteCmdLine)
+			stdout, _ := runScript(getRouteCmdLine)
 
-			if stdout == "" {
-				newRouteCmdLine := fmt.Sprintf("new-netroute -InterfaceIndex %v -DestinationPrefix %v -NextHop  %v -Verbose", route.linkIndex, route.destinationSubnet.String(), route.gatewayAddress.String())
-				runScript(newRouteCmdLine)
+			existingRoutes := parseRoutesList(stdout)
+			if len(existingRoutes) > 0 {
+				if routeEqual(existingRoutes[0], route) {
+					continue
+				}
+
+				log.Warningf("Replacing existing route to %v via %v with %v via %v.", evt.Lease.Subnet, existingRoutes[0].gatewayAddress, evt.Lease.Subnet, evt.Lease.Attrs.PublicIP)
+				removeRouteCmdLine := fmt.Sprint("remove-netroute -InterfaceIndex %v -DestinationPrefix %v -NextHop  %v -Verbose", route.linkIndex, route.destinationSubnet.String(), existingRoutes[0].gatewayAddress.String())
+				_, err := runScript(removeRouteCmdLine)
+				if err != nil {
+					log.Errorf("Error removing route: %v", err)
+					continue
+				}
+			}
+
+			newRouteCmdLine := fmt.Sprintf("new-netroute -InterfaceIndex %v -DestinationPrefix %v -NextHop  %v -Verbose", route.linkIndex, route.destinationSubnet.String(), route.gatewayAddress.String())
+			_, err := runScript(newRouteCmdLine)
+			if err != nil {
+				log.Errorf("Error creating route: %v", err)
 			}
 
 			n.addToRouteList(route)
@@ -147,17 +145,20 @@ func (n *network) handleSubnetEvents(batch []subnet.Event) {
 			}
 
 			route := route{
-				destinationSubnet:       evt.Lease.Subnet.ToIPNet(),
-				gatewayAddress:        evt.Lease.Attrs.PublicIP.ToIP(),
-				linkIndex: n.linkIndex,
+				destinationSubnet: evt.Lease.Subnet.ToIPNet(),
+				gatewayAddress:    evt.Lease.Attrs.PublicIP.ToIP(),
+				linkIndex:         n.linkIndex,
 			}
 
 			getRouteCmdLine := fmt.Sprint("get-netroute -InterfaceIndex %v -DestinationPrefix %v -erroraction Ignore", route.linkIndex, route.destinationSubnet.String())
-			stdout := runScript(getRouteCmdLine)
+			stdout, _ := runScript(getRouteCmdLine)
 
 			if stdout != "" {
-				newRouteCmdLine := fmt.Sprint("remove-netroute -InterfaceIndex %v -DestinationPrefix %v -NextHop  %v -Verbose", route.linkIndex, route.destinationSubnet.String(), route.gatewayAddress.String())
-				runScript(newRouteCmdLine)
+				removeRouteCmdLine := fmt.Sprint("remove-netroute -InterfaceIndex %v -DestinationPrefix %v -NextHop  %v -Verbose", route.linkIndex, route.destinationSubnet.String(), route.gatewayAddress.String())
+				_, err := runScript(removeRouteCmdLine)
+				if err != nil {
+					log.Errorf("Error removing route: %v", err)
+				}
 			}
 
 			n.removeFromRouteList(route)
@@ -198,13 +199,101 @@ func (n *network) routeCheck(ctx context.Context) {
 }
 
 func (n *network) checkSubnetExistInRoutes() {
+	getRouteCmdLine := "get-netroute -erroraction Ignore"
+	stdout, err := runScript(getRouteCmdLine)
+	if err != nil {
+		log.Errorf("Error enumerating routes", err)
+		return
+	}
+	currentRoutes := parseRoutesList(stdout)
+	for _, r := range n.rl {
+		exist := false
+		for _, currentRoute := range currentRoutes {
+			if routeEqual(r, currentRoute) {
+				exist = true
+				break
+			}
+		}
+
+		if !exist {
+			newRouteCmdLine := fmt.Sprintf("new-netroute -InterfaceIndex %v -DestinationPrefix %v -NextHop  %v -Verbose", r.linkIndex, r.destinationSubnet.String(), r.gatewayAddress.String())
+			_, err := runScript(newRouteCmdLine)
+			if err != nil {
+				log.Errorf("Error recovering route %v (%v)", newRouteCmdLine, err)
+				continue
+			}
+			log.Errorf("Recovered route %v", newRouteCmdLine)
+		}
+	}
+}
+
+func parseRoutesList(stdout string) []route {
+	internalWhitespaceRegEx := regexp.MustCompile(`[\s\p{Zs}]{2,}`)
+	scanner := bufio.NewScanner(strings.NewReader(stdout))
+	var routes []route
+	for scanner.Scan() {
+		line := internalWhitespaceRegEx.ReplaceAllString(scanner.Text(), "|")
+		if strings.HasPrefix(line, "ifIndex") || strings.HasPrefix(line, "----") {
+			continue
+		}
+
+		parts := strings.Split(line, "|")
+		if len(parts) != 5 {
+			continue
+		}
+
+		linkIndex, err := strconv.Atoi(parts[0])
+		if err != nil {
+			continue
+		}
+
+		gatewayAddress := net.ParseIP(parts[2])
+		if gatewayAddress == nil {
+			continue
+		}
+
+		_, destinationSubnet, err := net.ParseCIDR(parts[1])
+		if err != nil {
+			continue
+		}
+		route := route{
+			destinationSubnet: destinationSubnet,
+			gatewayAddress:    gatewayAddress,
+			linkIndex:         linkIndex,
+		}
+
+		routes = append(routes, route)
+	}
+
+	return routes
 }
 
 func routeEqual(x, y route) bool {
-	// TODO: rakesh: compare mask also?
-	if x.destinationSubnet.IP.Equal(y.destinationSubnet.IP) {
+	if x.destinationSubnet.IP.Equal(y.destinationSubnet.IP) && x.gatewayAddress.Equal(y.gatewayAddress) && bytes.Equal(x.destinationSubnet.Mask, y.destinationSubnet.Mask) {
 		return true
 	}
 
 	return false
+}
+
+func runScript(cmdLine string) (string, error) {
+	// choose a PS backend
+	back := &psbe.Local{}
+
+	// start a local powershell process
+	shell, err := ps.New(back)
+	if err != nil {
+		return "", err
+	}
+	defer shell.Exit()
+
+	log.Infof("Executing: %v", cmdLine)
+	stdout, stderr, err := shell.Execute(cmdLine)
+	if err != nil {
+		return "", err
+	}
+
+	log.Infof("stdout [%v], stderr [%v]", stdout, stderr)
+
+	return stdout, nil
 }
